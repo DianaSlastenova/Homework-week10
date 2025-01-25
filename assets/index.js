37db88cdec4a45bd1aecaee47018c8b78b48f84d
DianaSlastenova <d.slastenova@gmail.com>
2025-01-25 19:52:54 +0300

    add homework

---
+++ assets/index.js
@@ -0,0 +1,13 @@
+function writeText() {
+    console.log('Я учу JavaScript!');
+}
+writeText();
+
+const image = document.getElementById('cat1');
+    
+function changeImage1() {
+    image.src = './assets/img/cat2.jpg'
+}
+function changeImage2() {
+    image.src = './assets/img/cat1.jpg'
+}
